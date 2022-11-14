2e48954df5b7c3dc99df8b74dd98694c2a4ac83e
Carlos Eduardo Nicodemo <cadu.nicodemo@gmail.com>
2022-11-14 19:12:25 -0300

    Update
    Added Go! so teacher knows the timer is running

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
 function mudaTexto(text) {
-
     var mensagem = document.querySelector(".mensagem")
-
     mensagem.innerHTML = text;
 }
 
 function calculaTempo() {
     var timer = Math.floor(Math.random() * (75000 - 30000 + 1) + 30000);
     console.log(timer);
-
     setTimeout(function() {
         mudaTexto("Time's up!");
         document.querySelector(".timer__reset").classList.add("ativado");
         const alarm = new Audio('008724855-buzzer-game-show.m4a');
         alarm.play()
+        document.querySelector(".go").classList.add("contando");
     }, timer);
 }
 
-calculaTempo()
-
 function reseta() {
-    mudaTexto("Last Word!")
-    calculaTempo()
+    mudaTexto("Last Word!");
+    calculaTempo();
 }
 
 const reset = document.querySelector(".timer__reset");
+const funcionando = document.querySelector(".contando");
 reset.addEventListener('click', (e) => {
     reset.classList.remove("ativado");
+    funcionando.classList.remove("contando");
     reseta()
 });
