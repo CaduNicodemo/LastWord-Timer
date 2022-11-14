788df1610987faea0ea23ab31874a6dc183627f9
Carlos Eduardo Nicodemo <95039221+CaduNicodemo@users.noreply.github.com>
2022-11-14 18:35:08 -0300

    Upload files

---
+++ main.js
@@ -0,0 +1,31 @@
+function mudaTexto(text) {
+
+    var mensagem = document.querySelector(".mensagem")
+
+    mensagem.innerHTML = text;
+}
+
+function calculaTempo() {
+    var timer = Math.floor(Math.random() * (5000 - 3000 + 1) + 3000);
+    console.log(timer);
+
+    setTimeout(function() {
+        mudaTexto("Time's up!");
+        document.querySelector(".timer__reset").classList.add("ativado");
+        const alarm = new Audio('008724855-buzzer-game-show.m4a');
+        alarm.play()
+    }, timer);
+}
+
+calculaTempo()
+
+function reseta() {
+    mudaTexto("Last Word!")
+    calculaTempo()
+}
+
+const reset = document.querySelector(".timer__reset");
+reset.addEventListener('click', (e) => {
+    reset.classList.remove("ativado");
+    reseta()
+});
